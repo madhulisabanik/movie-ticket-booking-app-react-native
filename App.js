a8bae421c43e69afef20c1b69e671cd904d71f16
madhulisa <rbanik023@gmail.com>
2023-03-20 22:37:34 +0530

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { MovieContext } from "./Context";
+import StackNavigator from "./StackNavigator";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <MovieContext>
+        <StackNavigator />
+        <StatusBar style="auto" />
+      </MovieContext>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
---
+++ data/movies.js
@@ -0,0 +1,80 @@
+export default [
+    {
+      id: "0",
+      image:
+        "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OC45LzEwICA0LjdLIHZvdGVz,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00319185-recfcktmbq-portrait.jpg",
+      name: "Mrs. Chatterjee Vs Norway",
+      language: "Hindi",
+      genre: "DRAMA",
+    },
+    {
+      id: "1",
+      image:
+        "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OC41LzEwICA1LjVLIHZvdGVz,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00354652-arlmzwpbbd-portrait.jpg",
+      name: "Demon Slayer: Kimetsu no Yaiba",
+      language: "Japanese",
+      genre: "ACTION",
+    },
+    {
+      id: "2",
+      image:
+        "https://www.themoviedb.org/t/p/w440_and_h660_face/6SYaaIjLDistEd7DoeS328FVWn0.jpg",
+      name: "Seetharamam",
+      language: "Telugu",
+      genre: "DRAMA",
+    },
+    {
+      id: "3",
+      image:
+        "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ykefXRqhM7HVnLWPtXWcWCcvBy6.jpg",
+      name: "Raksha Bandhan",
+      language: "Hindi",
+      genre: "ACTION",
+    },
+    {
+      id: "4",
+      image:
+        "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OC40LzEwICA2LjdLIHZvdGVz,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00312239-nsutuudumm-portrait.jpg",
+      name: "Shazam! Fury of the Gods",
+      language: "English",
+      genre: "ACTION",
+    },
+    {
+      id: "5",
+      image:
+        "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/a2oUkUgMowYyi8qeGS28rAXPNXT.jpg",
+      name: "Kuruthi Aattam",
+      language: "Tamil",
+      genre: "ACTION",
+    },
+    {
+      id: "6",
+      image:
+        "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OS4xLzEwICA2LjlLIHZvdGVz,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00315054-ursehmwujh-portrait.jpg",
+      name: "Kabzaa",
+      language: "Kannada",
+      genre: "ACTION",
+    },
+    {
+      id: "7",
+      image:
+        "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/qlsNqqczg1rr5lfUgiPlLiykGGM.jpg",
+      name: "Viruman",
+      language: "Tamil",
+      genre: "DRAMA",
+    },
+    {
+        id:"8",
+        image:"https://www.themoviedb.org/t/p/w440_and_h660_face/xAG7S6X73B74CeLjo8gcVoWtunr.jpg",
+        name:"Rocketry",
+        language:"Hindi",
+        genre: "DRAMA",
+    },
+    {
+        id:"9",
+        image:"https://www.themoviedb.org/t/p/w188_and_h282_bestv2/irmHAELoYRaECBr8zQAlD4myrfk.jpg",
+        name:"777 Charlie",
+        language: "Kannada",
+        genre: "DRAMA",
+    }
+  ];
